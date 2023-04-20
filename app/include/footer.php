e9919b0f007bd7c355e0c71d0be09799468c0546
EkSev <strixstudy@gmail.com>
2023-04-20 21:53:25 +0300

    optimized site structure

---
+++ app/include/footer.php
@@ -0,0 +1,62 @@
+<div class="footer container-fluid">
+    <div class="footer-content container">
+        <div class="row">
+            <div class="footer-section about col-md-4 col-12">
+                <h3 class="logo-text">Блог</h3>
+                <p>
+                    Блог - это блог сделанный с целью обучения.
+                </p>
+                <div class="contact">
+                    <span><i class="fas fa-phone"></i>&nbsp; 89001112233</span>
+                    <span><i class="fas fa-envelope"></i>&nbsp; info@techtalk.com</span>
+                </div>
+                <div class="socials">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                </div>
+            </div>
+
+            <div class="footer-section links col-md-4 col-12">
+                <h3>Quick Links</h3>
+                <br>
+                <ul>
+                    <a href="#">
+                        <li>События</li>
+                    </a>
+                    <a href="#">
+                        <li>Команда</li>
+                    </a>
+                    <a href="#">
+                        <li>Упражнения</li>
+                    </a>
+                    <a href="#">
+                        <li>Галлерея</li>
+                    </a>
+                    <a href="#">
+                        <li>Что-то еще</li>
+                    </a>
+                </ul>
+            </div>
+
+            <div class="footer-section contact-form col-md-4 col-12">
+                <h3>Контакты</h3>
+                <br>
+                <form action="index.html" method="post">
+                    <input type="email" name="email" class="text-input contact-input" placeholder="Your email address...">
+                    <textarea rows="4" name="message" class="text-input contact-input" placeholder="Your message..."></textarea>
+                    <button type="submit" class="bth bth-big contact-btn">
+                        <i class="fas fa-envelope"></i>
+                        Отправить
+                    </button>
+                </form>
+            </div>
+
+        </div>
+
+        <div class="footer-bottom">
+            &copy; techtalkhub.com
+        </div>
+    </div>
+</div>
--- log.php
+++ log.php
@@ -1,3 +1,5 @@
+<?php include("path.php"); ?>
+
 <!doctype html>
 <html lang="ru">
 <head>
@@ -17,35 +19,7 @@
 </head>
 <body> 
 
-<header class="container-fluid">
-    <div class="container">
-        <div class="row">
-            <div class="col-4">
-                <h1>
-                    <a href="#">TechTalk Hub</a>
-                </h1>
-            </div>
-            <nav class="col-8">
-                <ul>
-                    <li><a href="#">Главная</a></li>
-                    <li><a href="#">О нас</a></li>
-                    <li><a href="#">Услуги</a></li>
-
-                    <li>
-                        <a href="#">
-                            <i class="fa fa-user"></i>
-                            Кабинет
-                        </a>
-                        <ul>
-                            <li><a href="#">Админ панель</a></li>
-                            <li><a href="#">Выход</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </div>
-</header>
+<?php include("app/include/header.php"); ?>
 <!-- END HEADER-->
 <!-- FORM -->
 <div class="container reg_form">
@@ -70,68 +44,7 @@
 <!-- END FORM-->
 
 <!-- footer -->
-<div class="footer container-fluid">
-    <div class="footer-content container">
-        <div class="row">
-            <div class="footer-section about col-md-4 col-12">
-                <h3 class="logo-text">Блог</h3>
-                <p>
-                    Блог - это блог сделанный с целью обучения.
-                </p>
-                <div class="contact">
-                    <span><i class="fas fa-phone"></i>&nbsp; 89001112233</span>
-                    <span><i class="fas fa-envelope"></i>&nbsp; info@techtalk.com</span>
-                </div>
-                <div class="socials">
-                    <a href="#"><i class="fab fa-facebook"></i></a>
-                    <a href="#"><i class="fab fa-instagram"></i></a>
-                    <a href="#"><i class="fab fa-twitter"></i></a>
-                    <a href="#"><i class="fab fa-youtube"></i></a>
-                </div>
-            </div>
-
-            <div class="footer-section links col-md-4 col-12">
-                <h3>Quick Links</h3>
-                <br>
-                <ul>
-                    <a href="#">
-                        <li>События</li>
-                    </a>
-                    <a href="#">
-                        <li>Команда</li>
-                    </a>
-                    <a href="#">
-                        <li>Упражнения</li>
-                    </a>
-                    <a href="#">
-                        <li>Галлерея</li>
-                    </a>
-                    <a href="#">
-                        <li>Что-то еще</li>
-                    </a>
-                </ul>
-            </div>
-
-            <div class="footer-section contact-form col-md-4 col-12">
-                <h3>Контакты</h3>
-                <br>
-                <form action="index.html" method="post">
-                    <input type="email" name="email" class="text-input contact-input" placeholder="Your email address...">
-                    <textarea rows="4" name="message" class="text-input contact-input" placeholder="Your message..."></textarea>
-                    <button type="submit" class="bth bth-big contact-btn">
-                        <i class="fas fa-envelope"></i>
-                        Отправить
-                    </button>
-                </form>
-            </div>
-
-        </div>
-
-        <div class="footer-bottom">
-            &copy; techtalkhub.com
-        </div>
-    </div>
-</div>
+<?php include("app/include/footer.php"); ?>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
