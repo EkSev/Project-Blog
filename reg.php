880ee872d67988c492128779efc86d6a696be6c0
EkSev <strixstudy@gmail.com>
2023-04-21 19:51:55 +0300

    Added data validation from the registration form

--- reg.php
+++ reg.php
@@ -25,16 +25,20 @@
 <!-- END HEADER-->
 <!-- FORM -->
 <div class="container reg_form">
-    <h2 class="col-12">Форма регистрации</h2>
     <form class="row justify-content-center" method="post" action="reg.php">
+        <h2>Форма регистрации</h2>
+        <div class="mb-3 col-12 col-md-4 err">
+            <p><?= $errMsg ?></p>
+        </div>
+        <div class="w-100"></div>
         <div class="mb-3 col-12 col-md-4">
             <label for="formGroupExampleInput" class="form-label">Ваш логин</label>
-            <input name="login" type="text" class="form-control" id="formGroupExampleInput" placeholder="Введите ваш логин...">
+            <input name="login" value="<?=$login?>" type="text" class="form-control" id="formGroupExampleInput" placeholder="Введите ваш логин...">
         </div>
         <div class="w-100"></div>
         <div class="mb-3 col-12 col-md-4">
             <label for="exampleInputEmail1" class="form-label">Email</label>
-            <input name="mail" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Введите ваш email...">
+            <input name="mail" value="<?=$email?>" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Введите ваш email...">
             <div id="emailHelp" class="form-text">Ваш email адрес не будет использован для спама!</div>
         </div>
         <div class="w-100"></div>
