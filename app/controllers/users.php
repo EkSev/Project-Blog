a1c0d804e41d369c916cb37c8ce7ef6c0ac39f50
EkSev <strixstudy@gmail.com>
2023-04-21 20:27:17 +0300

    added a check for the presence of a user in the database

--- app/controllers/users.php
+++ app/controllers/users.php
@@ -10,30 +10,30 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     $passF = trim($_POST['pass-first']);
     $passS = trim($_POST['pass-second']);
 
-
     if($login === '' || $email === '' || $passF === ''){
         $errMsg = "Не все поля заполнены!";
     }elseif (mb_strlen($login, 'UTF8') < 2){
-        $errMsg = "Логин должен быть более 2-х символов";
+        $errMsg = "Логин должен быть более 2-х символов!";
     }elseif ($passF !== $passS){
         $errMsg = "Пароли в обоих полях должны соответствовать!";
     }else{
-        $pass = password_hash($passF, PASSWORD_DEFAULT);
-        $post = [
-            'admin' => $admin,
-            'username' => $login,
-            'email' => $email,
-            'password' => $pass
-        ];
-        $id = insert('users', $post);
+        $existence = selectOne('users', ['email' => $email]);
+        if (!empty($existence['email']) && $existence['email'] === $email){
+            $errMsg = "Пользователь с такой почтой уже зарегистрирован!";
+        }else{
+            $pass = password_hash($passF, PASSWORD_DEFAULT);
+            $post = [
+                'admin' => $admin,
+                'username' => $login,
+                'email' => $email,
+                'password' => $pass
+            ];
+            $id = insert('users', $post);
+            $errMsg = "Пользователь " . "<strong>" . $login . "</strong>" . " успешно зарегистрирован!";
+        }
     }
-
-    // $last_row = selectOne('users', ['id' => $id]);
 }else{
-    echo 'GET';
     $login = '';
     $email = '';
 }
 
-
-//    $pass = password_hash($_POST['pass-second'], PASSWORD_DEFAULT);
