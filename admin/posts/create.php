91aa7b9d87264414cd322bea51cfe7599c583c90
EkSev <strixstudy@gmail.com>
2023-04-23 20:28:20 +0300

    creating an entry (blog post) in the admin panel

--- admin/posts/create.php
+++ admin/posts/create.php
@@ -1,6 +1,6 @@
 <?php 
-session_start();
 include("../../path.php"); 
+include("../../app/controllers/posts.php");
 ?>
 
 <!doctype html>
@@ -38,24 +38,24 @@ include("../../path.php");
             <div class="row add-post">
                 <form action="create.php" method="post">
                     <div class="col">
-                        <input type="text" class="form-control" placeholder="Title" aria-label="Название статьи">
+                        <input name="title" type="text" class="form-control" placeholder="Title" aria-label="Название статьи">
                     </div>
                     <div class="col">
                         <label for="content" class="form-label">Содержимое записи</label>
-                        <textarea class="form-control" id="content" rows="6"></textarea>
+                        <textarea name="content" class="form-control" id="content" rows="6"></textarea>
                     </div>
                     <div class="input-group col">
-                        <input type="file" class="form-control" id="inputGroupFile02">
+                        <input name="img" type="file" class="form-control" id="inputGroupFile02">
                         <label class="input-group-text" for="inputGroupFile02">Upload</label>
                     </div>
-                    <select class="form-select" aria-label="Default select example">
-                        <option selected>Open this select menu</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                    <select name="topic" class="form-select" aria-label="Default select example">
+                        <option selected>Категория поста:</option>
+                        <?php foreach($topics as $key => $topic): ?>
+                            <option value="<?= $topic['id']; ?>"><?= $topic['name']; ?></option>
+                        <?php endforeach; ?>
                     </select>
                     <div class="col">
-                        <button class="btn btn-primary" type="submit">Сохранить запись</button>
+                        <button name="add_post" class="btn btn-primary" type="submit">Добавить запись</button>
                     </div>
                 </form>
             </div>
---
+++ app/controllers/posts.php
@@ -0,0 +1,40 @@
+<?php
+include(SITE_ROOT . "/app/database/db.php");
+
+$errMsg = '';
+$id = '';
+$title = '';
+$content = '';
+$img = '';
+$topic = '';
+
+$topics = selectAll('topics');
+
+
+// Код для формы создания записи
+if($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_post'])){
+
+    $title = trim($_POST['title']);
+    $content = trim($_POST['content']);
+    $topic = trim($_POST['topic']);
+    if($title === '' || $content === '' || $topic === ''){
+        $errMsg = "Не все поля заполнены!";
+    }elseif (mb_strlen($title, 'UTF8') < 7){
+        $errMsg = "Название статьи должно быть более 7-х символов!";
+    }else{
+        $post = [
+            'id_user' => $_SESSION['id'],
+            'title' => $title,
+            'content' => $content,
+            'img' => $_POST['img'],
+            'status' => 1,
+            'id_topic' => $topic
+        ];
+        $post = insert('posts', $post);
+        $post = selectOne('posts', ['id' => $id]);
+        header('location: ' . BASE_URL . 'admin/posts/index.php');
+    }
+}else{
+    $title = '';
+    $content = '';
+}
